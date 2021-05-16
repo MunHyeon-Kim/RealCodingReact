0947b0a96da38842638428bf6579d08cc9f08e75
huring <qbarche38@naver.com>
2021-05-16 23:15:11 +0900

    homework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './pages/About'
+import Home from './pages/Home'
+import Cities from './pages/Cities'
 
 function App() {
+  const city = "Seoul";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <div className="App">
+         <Header region="KR" cityName={city} />
+         <ul className="menu">
+           <li>
+             <Link to="/">Home</Link></li>
+           <li>
+             <Link to="/About">About</Link></li>
+           <li>
+           <Link to="/cities">Cities</Link></li>
+         </ul>
+
+         <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/cities">
+            <Cities />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return(
+        <div>
+            <h1>About</h1>
+        </div>
+    )
+
+}
+
+export default Home;
